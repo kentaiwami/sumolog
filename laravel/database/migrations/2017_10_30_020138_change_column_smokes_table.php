c1c1deb37052571891e09bf901b22e351573f548
Kenta Iwami <g2117009@fun.ac.jp>
2017-10-30 02:09:35 +0900

    ユーザとスモークテーブルの定義

---
+++ laravel/database/migrations/2017_10_30_020138_change_column_smokes_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ChangeColumnSmokesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('smokes', function (Blueprint $table) {
+            $table->dateTime('ended_at')->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('smokes', function (Blueprint $table) {
+            $table->dateTime('ended_at')->nullable()->change();
+        });
+    }
+}
