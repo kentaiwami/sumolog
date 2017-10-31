e67d41a61af28a11dd8d90794341cfdbc6f9ee28
Kenta Iwami <g2117009@fun.ac.jp>
2017-10-31 22:36:31 +0900

    endpoint変更

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -14,10 +14,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-//Route::middleware('auth:api')->get('/user', function (Request $request) {
-//    return 'HOGE';
-////    return $request->user();
-//});
 
-//Route::resource('/store_smoke', 'Resource\\StoreUserController');
-Route::post('store_smoke', 'StoreUserController@store');
+Route::post('user', 'StoreUserController@store');
