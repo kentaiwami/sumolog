071b65c1ab22dbaee86d226dcb0652a11a2838d0
Kenta Iwami <g2117009@fun.ac.jp>
2017-11-01 16:25:56 +0900

    喫煙開始のAPIを実装

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -16,3 +16,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('user', 'APIUserController@store');
 Route::put('user/{uuid}', 'APIUserController@update');
+
+Route::post('smoke', 'APISmokeController@store');
