43a04af8360701ea9d4366da6513de209b1dc9a8
Kenta Iwami <g2117009@fun.ac.jp>
2018-01-02 02:21:00 +0900

    save uuidのAPIを作成

--- raspberry-pi/api/api.py
+++ raspberry-pi/api/api.py
@@ -1,6 +1,24 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///sumolog.db'
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    uuid = db.Column(db.String(256), unique=True)
+
+    def __init__(self, uuid):
+        self.uuid = uuid
+
+    def to_dict(self):
+        return dict(uuid=self.uuid)
+
+
+def db_create():
+    db.create_all()
 
 
 @app.route('/')
@@ -8,5 +26,16 @@ def hello_world():
     return 'Hello World!'
 
 
+@app.route("/api/v1/user", methods=['POST'])
+def api_v1_models():
+    if request.method == 'POST':
+        uuid = request.json['uuid']
+        d = User(uuid)
+        db.session.add(d)
+        db.session.commit()
+        return jsonify(d.to_dict()), 200
+
+
 if __name__ == '__main__':
+    db_create()
     app.run(host='0.0.0.0', port=80, debug=True)
