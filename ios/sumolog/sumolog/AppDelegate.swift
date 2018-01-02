0c6f48cbb4543a82b1aca884247df93fd52b17ac
Kenta Iwami <g2117009@fun.ac.jp>
2018-01-02 15:04:46 +0900

    起動時にuuidが設定されているかを判断して画面を切り替える処理を実装

--- ios/sumolog/sumolog/AppDelegate.swift
+++ ios/sumolog/sumolog/AppDelegate.swift
@@ -7,15 +7,23 @@
 //
 
 import UIKit
+import KeychainAccess
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        let keychain = Keychain()
+        let key = try! keychain.getString("uuid")
+        
+        if key == nil {
+            let settingVC = SettingViewController()
+            self.window?.rootViewController = settingVC
+            self.window?.makeKeyAndVisible()
+        }
+        
         return true
     }
 
---
+++ ios/sumolog/sumolog/SettingViewController.swift
@@ -0,0 +1,37 @@
+//
+//  SettingViewController.swift
+//  sumolog
+//
+//  Created by 岩見建汰 on 2018/01/02.
+//  Copyright © 2018年 Kenta. All rights reserved.
+//
+
+import UIKit
+
+class SettingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = UIColor.blue
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
