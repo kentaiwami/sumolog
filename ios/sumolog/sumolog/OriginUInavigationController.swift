6f530cacc19eec3d556709d9048bfeb3d6e3cedc
Kenta Iwami <g2117009@fun.ac.jp>
2018-01-03 05:38:42 +0900

    ナビゲーションコントローラーの設定用ファイルを追加

---
+++ ios/sumolog/sumolog/OriginUInavigationController.swift
@@ -0,0 +1,25 @@
+//
+//  OriginUInavigationController.swift
+//  sumolog
+//
+//  Created by 岩見建汰 on 2018/01/03.
+//  Copyright © 2018年 Kenta. All rights reserved.
+//
+
+import UIKit
+
+class OriginUInavigationController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        navigationBar.barTintColor = UIColor.hex(Color.main.rawValue as NSString, alpha: 1.0)
+        navigationBar.titleTextAttributes = [NSForegroundColorAttributeName: UIColor.white]
+        self.navigationBar.tintColor = UIColor.white
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
