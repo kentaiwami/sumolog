edc29ffa20fa3bb50447cf779388025a2dd95b01
Kenta Iwami <g2117009@fun.ac.jp>
2018-01-06 00:44:47 +0900

    ロジックのベースを記述

--- ios/sumolog/sumolog/EditViewController.swift
+++ ios/sumolog/sumolog/EditViewController.swift
@@ -15,16 +15,49 @@ import KeychainAccess
 class EditViewController: UIViewController {
 
     var iscreated_form = false
+    let indicator = Indicator()
+    var id = ""
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
         self.tabBarController?.navigationItem.title = "Edit"
+        
+        CallGet24HourSmokeAPI()
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        let keychain = Keychain()
+        id = (try! keychain.getString("id"))!
+    }
+    
+    func CallGet24HourSmokeAPI() {
+        let urlString = API.base.rawValue + API.v1.rawValue + API.smoke.rawValue + API.detail.rawValue + API.user.rawValue + id
+        indicator.showIndicator(view: self.view)
+        
+        Alamofire.request(urlString, method: .get).responseJSON { (response) in
+            self.indicator.stopIndicator()
+            
+            guard let object = response.result.value else{return}
+            let json = JSON(object)
+            print("Smoke 24hour results: ", json.count)
+            print(json)
+            
+            if !self.iscreated_form {
+                self.CreateForms()
+            }
+            self.UpdateCells()
+        }
+    }
+    
+    func CreateForms() {
+        iscreated_form = true
+    }
+    
+    func UpdateCells() {
+        
     }
 
     override func didReceiveMemoryWarning() {
