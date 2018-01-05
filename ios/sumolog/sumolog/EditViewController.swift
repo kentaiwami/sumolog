51da7b899de5f5b08cd500856f4e29b32706f724
Kenta Iwami <g2117009@fun.ac.jp>
2018-01-05 21:40:28 +0900

    editのvc追加、色を設定

---
+++ ios/sumolog/sumolog/EditViewController.swift
@@ -0,0 +1,28 @@
+//
+//  EditViewController.swift
+//  sumolog
+//
+//  Created by 岩見建汰 on 2018/01/05.
+//  Copyright © 2018年 Kenta. All rights reserved.
+//
+
+import UIKit
+
+class EditViewController: UIViewController {
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        self.tabBarController?.navigationItem.title = "Edit"
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
