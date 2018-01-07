042d6891b2bef31aa44b46198722a93137ee16f7
Kenta Iwami <g2117009@fun.ac.jp>
2018-01-07 23:47:25 +0900

    smokeカウントまで追加

--- ios/sumolog/sumolog/SmokeOverViewViewController.swift
+++ ios/sumolog/sumolog/SmokeOverViewViewController.swift
@@ -18,10 +18,11 @@ class SmokeOverViewViewController: UIViewController, ScrollableGraphViewDataSour
     let indicator = Indicator()
     var id = ""
     
-    var latest_minLabel = UILabel()
-    var minLabel = UILabel()
+    var latestLabel = UILabel()
+    var aveLabel = UILabel()
     var smoke_countLabel = UILabel()
-    var smokeImageView = UIImageView()
+    var descriptionLabel: [UILabel] = []
+    var borderView: [UIView] = []
     var graphView = ScrollableGraphView()
     
     override func viewWillAppear(_ animated: Bool) {
@@ -57,46 +58,126 @@ class SmokeOverViewViewController: UIViewController, ScrollableGraphViewDataSour
     }
     
     func DrawViews() {
-        latest_minLabel.removeFromSuperview()
-        minLabel.removeFromSuperview()
+        latestLabel.removeFromSuperview()
+        aveLabel.removeFromSuperview()
+        
+        for label in descriptionLabel {
+            label.removeFromSuperview()
+        }
+        descriptionLabel.removeAll()
+        
+        for view in borderView {
+            view.removeFromSuperview()
+        }
+        borderView.removeAll()
         smoke_countLabel.removeFromSuperview()
-        smokeImageView.removeFromSuperview()
-        graphView.removeFromSuperview()
+//        smokeImageView.removeFromSuperview()
+//        graphView.removeFromSuperview()
         
-        CreateLatestMinLabel()
-        CreateMinLabel()
+        CreateLatestLabel()
+        CreateDescriptionLabel(str: "Latest", target: latestLabel)
+        CreateAveLabel()
+        CreateDescriptionLabel(str: "Ave", target: aveLabel)
+        CreateBorderView(target: descriptionLabel.last!)
         CreateSumSmokesCountLabel()
-        CreateSmokeImageView()
-        CreateGraphView()
-        
-        GenerateAlert()
+        CreateDescriptionLabel(str: "24hour smoked", target: smoke_countLabel)
+//        CreateSmokeImageView()
+//        CreateGraphView()
+//
+//        GenerateAlert()
     }
     
-    func CreateLatestMinLabel() {
-        let label = UILabel(frame: CGRect.zero)
-        label.font = UIFont(name: Font.HiraginoW3.rawValue, size: 100)
-        label.textColor = UIColor.hex(Color.gray.rawValue, alpha: 1.0)
-        label.text = String(data.GetMin())
+    func CreateLatestLabel() {
+        var h = 0
+        var m = 0
+        var str = ""
+        let min = data.GetMin()
+        if min >= 60 {
+            h = min / 60
+            m = min % 60
+            
+            if m == 0 {
+                str = String(h)+"h"
+            }else {
+                str = String(h)+"h"+String(m)+"m"
+            }
+        }else {
+            m = min
+            str = String(m)+"m"
+        }
         
-        latest_minLabel = label
+        let label = UILabel(frame: CGRect.zero)
+        label.font = UIFont(name: Font.HiraginoW3.rawValue, size: 60)
+        label.textColor = UIColor.hex(Color.gray.rawValue, alpha: 1.0)
+        label.attributedText = GetAttrString(str: str)
+        
+        latestLabel = label
         
         self.view.addSubview(label)
         
-        label.center(in: self.view, offset: CGPoint(x: 0, y: -100))
+        label.topToBottom(of: (self.navigationController?.navigationBar)!, offset: 25)
+        label.centerX(to: self.view, offset: -100)
     }
     
-    func CreateMinLabel() {
+    func CreateAveLabel() {
         let label = UILabel(frame: CGRect.zero)
-        label.font = UIFont(name: Font.HiraginoW6.rawValue, size: 30)
+        label.font = UIFont(name: Font.HiraginoW3.rawValue, size: 60)
         label.textColor = UIColor.hex(Color.gray.rawValue, alpha: 1.0)
-        label.text = "min"
+        label.attributedText = GetAttrString(str: String(data.GetAve()) + "m")
         
-        minLabel = label
+        aveLabel = label
         
         self.view.addSubview(label)
         
-        label.topToBottom(of: latest_minLabel, offset: -10)
-        label.leadingToTrailing(of: latest_minLabel, offset: -20)
+        label.topToBottom(of: (self.navigationController?.navigationBar)!, offset: 25)
+        label.centerX(to: self.view, offset: 100)
+    }
+    
+    func GetAttrString(str: String) -> NSAttributedString {
+        let attr_str = NSMutableAttributedString(string: str)
+        
+        let chars:[Character] = ["h", "m", "n", "u"]
+        
+        for char in chars {
+            let char_index = str.index(of: char)
+            
+            if let char_index = char_index {
+                let position = str.distance(from: str.startIndex, to: char_index).advanced(by: 0)
+                attr_str.addAttribute(NSFontAttributeName, value: UIFont(name: Font.HiraginoW3.rawValue, size: 30), range: NSRange(location: position, length: 1))
+            }
+        }
+        
+        return attr_str
+    }
+    
+    func CreateDescriptionLabel(str: String, target: UILabel) {
+        let attr_str = NSMutableAttributedString(string: str)
+        attr_str.addAttribute(NSFontAttributeName, value: UIFont(name: Font.HiraginoW3.rawValue, size: 15), range: NSRange(location: 0, length: attr_str.length))
+        
+        let label = UILabel(frame: CGRect.zero)
+        label.font = UIFont(name: Font.HiraginoW3.rawValue, size: 60)
+        label.textColor = UIColor.hex(Color.gray.rawValue, alpha: 0.5)
+        label.attributedText = attr_str
+        
+        self.view.addSubview(label)
+        
+        descriptionLabel.append(label)
+        
+        label.topToBottom(of: target)
+        label.centerX(to: target)
+    }
+    
+    func CreateBorderView(target: UILabel) {
+        let view = UIView()
+        view.backgroundColor = UIColor.hex(Color.gray.rawValue, alpha: 0.5)
+        
+        self.view.addSubview(view)
+        
+        borderView.append(view)
+        
+        view.topToBottom(of: target)
+        view.height(2)
+        view.width(self.view.frame.width)
     }
     
     func CreateSumSmokesCountLabel() {
@@ -105,74 +186,60 @@ class SmokeOverViewViewController: UIViewController, ScrollableGraphViewDataSour
         if data.GetOver() > 0 {
             textColor = UIColor.red
         }
-        
+
         let label = UILabel(frame: CGRect.zero)
-        label.font = UIFont(name: Font.HiraginoW3.rawValue, size: 30)
+        label.font = UIFont(name: Font.HiraginoW3.rawValue, size: 60)
         label.textColor = textColor
-        label.text = String(data.GetCount())
-        
+        label.attributedText = GetAttrString(str: String(data.GetCount()) + "num")
+
         smoke_countLabel = label
-        
+
         self.view.addSubview(label)
-        
-        label.trailing(to: self.view, offset: -10)
-        label.topToBottom(of: minLabel, offset: 30)
+
+        label.topToBottom(of: borderView.last!, offset: 0)
+        label.centerX(to: self.view)
     }
     
-    func CreateSmokeImageView() {
-        let size = 30 as CGFloat
-        let imageView = UIImageView(image: UIImage(named: "icon_smoke"))
-        imageView.frame = CGRect.zero
-        
-        smokeImageView = imageView
-        
-        self.view.addSubview(imageView)
-        
-        imageView.trailingToLeading(of: smoke_countLabel)
-        imageView.centerY(to: smoke_countLabel)
-        imageView.width(size)
-        imageView.height(size)
-    }
     
-    func CreateGraphView() {
-        
-        let frame = CGRect.zero
-        let graphView = ScrollableGraphView(frame: frame, dataSource: self)
-        let barPlot = BarPlot(identifier: "bar")
-        
-        barPlot.barWidth = 5
-        barPlot.barLineWidth = 1
-        barPlot.barLineColor = UIColor.clear
-        barPlot.barColor = UIColor.hex(Color.main.rawValue, alpha: 1.0)
-        barPlot.adaptAnimationType = ScrollableGraphViewAnimationType.elastic
-        barPlot.animationDuration = 1.0
-        
-        let referenceLines = ReferenceLines()
-        referenceLines.referenceLineLabelFont = UIFont.boldSystemFont(ofSize: 10)
-        referenceLines.referenceLineColor = UIColor.hex(Color.gray.rawValue, alpha: 0.1)
-        referenceLines.referenceLineLabelColor = UIColor.hex(Color.gray.rawValue, alpha: 1.0)
-        referenceLines.dataPointLabelColor = UIColor.hex(Color.gray.rawValue, alpha: 1.0)
-        
-        
-        graphView.rangeMin = 0
-        graphView.rangeMax = CalcMaxRange()
-        graphView.backgroundFillColor = UIColor.white
-        graphView.shouldAnimateOnStartup = true
-        graphView.addPlot(plot: barPlot)
-        graphView.addReferenceLines(referenceLines: referenceLines)
-        graphView.direction = .rightToLeft
-        graphView.dataPointSpacing = 30
-        
-        self.graphView = graphView
-        
-        self.view.addSubview(graphView)
-        
-        graphView.width(to: self.view)
-        graphView.leading(to: self.view)
-        graphView.trailing(to: self.view)
-        graphView.topToBottom(of: smokeImageView, offset: 20)
-        graphView.bottom(to: self.view, offset: -80)
-    }
+//    func CreateGraphView() {
+//
+//        let frame = CGRect.zero
+//        let graphView = ScrollableGraphView(frame: frame, dataSource: self)
+//        let barPlot = BarPlot(identifier: "bar")
+//
+//        barPlot.barWidth = 5
+//        barPlot.barLineWidth = 1
+//        barPlot.barLineColor = UIColor.clear
+//        barPlot.barColor = UIColor.hex(Color.main.rawValue, alpha: 1.0)
+//        barPlot.adaptAnimationType = ScrollableGraphViewAnimationType.elastic
+//        barPlot.animationDuration = 1.0
+//
+//        let referenceLines = ReferenceLines()
+//        referenceLines.referenceLineLabelFont = UIFont.boldSystemFont(ofSize: 10)
+//        referenceLines.referenceLineColor = UIColor.hex(Color.gray.rawValue, alpha: 0.1)
+//        referenceLines.referenceLineLabelColor = UIColor.hex(Color.gray.rawValue, alpha: 1.0)
+//        referenceLines.dataPointLabelColor = UIColor.hex(Color.gray.rawValue, alpha: 1.0)
+//
+//
+//        graphView.rangeMin = 0
+//        graphView.rangeMax = CalcMaxRange()
+//        graphView.backgroundFillColor = UIColor.white
+//        graphView.shouldAnimateOnStartup = true
+//        graphView.addPlot(plot: barPlot)
+//        graphView.addReferenceLines(referenceLines: referenceLines)
+//        graphView.direction = .rightToLeft
+//        graphView.dataPointSpacing = 30
+//
+//        self.graphView = graphView
+//
+//        self.view.addSubview(graphView)
+//
+//        graphView.width(to: self.view)
+//        graphView.leading(to: self.view)
+//        graphView.trailing(to: self.view)
+//        graphView.topToBottom(of: smokeImageView, offset: 20)
+//        graphView.bottom(to: self.view, offset: -80)
+//    }
     
     func value(forPlot plot: Plot, atIndex pointIndex: Int) -> Double {
         let hour = data.GetHour()
