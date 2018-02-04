f38507d34e269a2d97b85e46773c99f094ced562
Kenta Iwami <g2117009@fun.ac.jp>
2018-02-04 13:24:57 +0900

    formをtextからdatetimeへ変更

--- ios/sumolog/sumolog/Edit/SmokeDataEditViewController.swift
+++ ios/sumolog/sumolog/Edit/SmokeDataEditViewController.swift
@@ -41,64 +41,23 @@ class SmokeDataEditViewController: FormViewController {
         }
     }
     
-    func CreateForms() {
-        var rules = RuleSet<String>()
-        rules.add(rule: RuleRequired(msg: "必須項目です"))
-        rules.add(rule: RuleRegExp(regExpr: "[0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2}", allowsEmpty: false, msg: "形式を確認してください。ex.) 2017-02-04 01:03:04"))
-        
-        LabelRow.defaultCellUpdate = { cell, row in
-            cell.contentView.backgroundColor = .red
-            cell.textLabel?.textColor = .white
-            cell.textLabel?.font = UIFont.boldSystemFont(ofSize: 13)
-            cell.textLabel?.textAlignment = .right
-        }
+    func CreateForms() {        
+        let dateFormatter = DateFormatter()
+        dateFormatter.timeZone = TimeZone.current
+        dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
         
         form +++ Section("喫煙情報")
-            <<< TextRow(){
-                $0.title = "Start Time"
-                $0.value = started_at
-                $0.add(ruleSet: rules)
-                $0.validationOptions = .validatesOnChange
+            <<< DateTimeRow(){
+                $0.title = "Start"
+                $0.value = dateFormatter.date(from: started_at)
                 $0.tag = "start"
-            }.onRowValidationChanged {cell, row in
-                let rowIndex = row.indexPath!.row
-                while row.section!.count > rowIndex + 1 && row.section?[rowIndex  + 1] is LabelRow {
-                    row.section?.remove(at: rowIndex + 1)
-                }
-                if !row.isValid {
-                    for (index, err) in row.validationErrors.map({ $0.msg }).enumerated() {
-                        let labelRow = LabelRow() {
-                            $0.title = err
-                            $0.cell.height = { 30 }
-                        }
-                        row.section?.insert(labelRow, at: row.indexPath!.row + index + 1)
-                    }
-                }
             }
         
-        
-            <<< TextRow(){
-                $0.title = "End Time"
-                $0.value = ended_at
-                $0.add(ruleSet: rules)
-                $0.validationOptions = .validatesOnChange
+            <<< DateTimeRow(){
+                $0.title = "End"
+                $0.value = dateFormatter.date(from: ended_at)
                 $0.tag = "end"
-            }.onRowValidationChanged {cell, row in
-                let rowIndex = row.indexPath!.row
-                while row.section!.count > rowIndex + 1 && row.section?[rowIndex  + 1] is LabelRow {
-                    row.section?.remove(at: rowIndex + 1)
-                }
-                if !row.isValid {
-                    for (index, err) in row.validationErrors.map({ $0.msg }).enumerated() {
-                        let labelRow = LabelRow() {
-                            $0.title = err
-                            $0.cell.height = { 30 }
-                        }
-                        row.section?.insert(labelRow, at: row.indexPath!.row + index + 1)
-                    }
-                }
             }
-        
         
         form +++ Section(header: "", footer: "入力された情報で上書きします")
             <<< ButtonRow(){
