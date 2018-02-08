ad694adb305fb8a324176369c13c2a3043328ec4
Kenta Iwami <g2117009@fun.ac.jp>
2018-02-08 15:21:36 +0900

    devicetoken表示を実装

--- ios/sumolog/sumolog/AppDelegate.swift
+++ ios/sumolog/sumolog/AppDelegate.swift
@@ -64,6 +64,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
         print(error)
     }
+    
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        let deviceToken = String(format: "%@", deviceToken as CVarArg) as String
+        print("deviceToken = \(deviceToken)")
+    }
 
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
