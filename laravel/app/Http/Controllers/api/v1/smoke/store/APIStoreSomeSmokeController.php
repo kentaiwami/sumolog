3f9c5b2507b97eb926a70813f31a197402450fe7
Kenta Iwami <g2117009@fun.ac.jp>
2018-08-17 10:59:55 +0900

    一旦

--- laravel/app/Http/Controllers/api/v1/smoke/store/APIStoreSomeSmokeController.php
+++ laravel/app/Http/Controllers/api/v1/smoke/store/APIStoreSomeSmokeController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\api\v1\smoke\store;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Validator;
+use App\Smoke;
+use App\User;
 
 
 class APIStoreSomeSmokeController extends Controller
@@ -20,12 +22,18 @@ class APIStoreSomeSmokeController extends Controller
         $validator = Validator::make($request->all(), [
             'uuid' => 'bail|required|regex:/^[0-9A-F]{8}-[0-9A-F]{4}-[0-9A-F]{4}-[0-9A-F]{4}-[0-9A-F]{12}$/',
             'start_point' => 'bail|required|regex:/^[0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2}$/',
-            'end_point' => 'bail|required|regex:/^[0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2}$/'
+            'end_point' => 'bail|required|regex:/^[0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2}$/',
+            'smoke_time' => 'bail|required|integer|min:1',
+            'smoke_count' => 'bail|required|integer|min:1'
         ]);
 
         if($validator->fails()){
             return Response()->json($validator->errors());
         }
+
+        $user = User::where('uuid', $request->get('uuid'))->firstOrFail();
+
+//
 
         return Response()->json([
             'uuid'      => 'uuid'
