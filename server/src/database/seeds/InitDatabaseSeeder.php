44f1c1cbc9f40244053db86e97289dd38bf5490a
Kenta Iwami <kentaiwami12@gmail.com>
2019-11-17 19:49:29 +0900

    seeder commentout

--- server/src/database/seeds/InitDatabaseSeeder.php
+++ server/src/database/seeds/InitDatabaseSeeder.php
@@ -3,13 +3,13 @@ use Illuminate\Database\Seeder;
 class InitDatabaseSeeder extends Seeder {
     public function run()
     {
-        $path = 'database/sql/01_admin_menu.sql';
-        DB::unprepared(file_get_contents($path));
+        // $path = 'database/sql/01_admin_menu.sql';
+        // DB::unprepared(file_get_contents($path));
 
-        $path = 'database/sql/02_users.sql';
-        DB::unprepared(file_get_contents($path));
+        // $path = 'database/sql/02_users.sql';
+        // DB::unprepared(file_get_contents($path));
 
-        $path = 'database/sql/03_smokes.sql';
-        DB::unprepared(file_get_contents($path));
+        // $path = 'database/sql/03_smokes.sql';
+        // DB::unprepared(file_get_contents($path));
     }
 }
